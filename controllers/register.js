fae2cfceb45ef097993dcb040fede758d35d869d
hkxxx843 <hkxxx843@gmail.com>
2021-05-09 21:05:31 +0530

    initial commit

---
+++ controllers/register.js
@@ -0,0 +1,36 @@
+const saltRounds = 10;
+
+const handleRegister = (db, bcrypt) => (req, res) => {
+  const { email, name, password } = req.body;
+  if(!email || !name || !password) {
+    return res.status(400).json("Incorrect form submisstion");
+  }
+  const hash = bcrypt.hashSync(password, saltRounds);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(() => res.status(400).json("unable to register"));
+};
+
+module.exports = {
+  handleRegister: handleRegister,
+};
